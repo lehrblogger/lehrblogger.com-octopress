cfd43a4b7f3f33e8b036580e92cab8193bc16b2a
Steven Lehrburger <lehrburger@gmail.com>
2013-06-19 02:27:41 -0400

    Manage/generate S3 RoutingRules w/ a python script
    I discovered I needed additional routing rules so that:
    	a) the "/itp/" rule wouldn't match on the legit "/itp/" category, and break things by redirecting to "/itp-"
    	b) the old case-insensitive category names would still mostly work, as long as the URL you were using matches canonical WordPress case
    
    The list was getting a bit on weildy, so I made a little script to print them out. I'm sure there's [a way to set them automatically](https://github.com/laurilehmijoki/configure-s3-website/pull/1), but it didn't seem worth setting up for now.

--- README.markdown
+++ README.markdown
@@ -1,56 +1,8 @@
 [lehrblogger.com](http://lehrblogger.com)
 ===============
-
 WordPress I love you, [but you're bringing me down.](http://www.youtube.com/watch?v=-eohHwsplvY)
 
 A static Jekyll site based off of the [Octopress framework](http://octopress.org/) and [Ryan Duessing's Minimalist theme.](https://github.com/ryandeussing/octopress-minimalist) and hosted on [Amazon S3](http://aws.amazon.com/s3/).
-
-S3 Redirection Rules
---------------------
-```xml
-<RoutingRules>
-    <RoutingRule>
-        <Condition>
-            <KeyPrefixEquals>nyu/projects</KeyPrefixEquals>
-        </Condition>
-        <Redirect>
-            <ReplaceKeyPrefixWith>projects</ReplaceKeyPrefixWith>
-        </Redirect>
-    </RoutingRule>
-    <RoutingRule>
-        <Condition>
-            <KeyPrefixEquals>nyu/events</KeyPrefixEquals>
-        </Condition>
-        <Redirect>
-            <ReplaceKeyPrefixWith>projects</ReplaceKeyPrefixWith>
-        </Redirect>
-    </RoutingRule>
-    <RoutingRule>
-        <Condition>
-            <KeyPrefixEquals>nyu/classes</KeyPrefixEquals>
-        </Condition>
-        <Redirect>
-            <ReplaceKeyPrefixWith>projects</ReplaceKeyPrefixWith>
-        </Redirect>
-    </RoutingRule>
-    <RoutingRule>
-        <Condition>
-            <KeyPrefixEquals>category/itp/</KeyPrefixEquals>
-        </Condition>
-        <Redirect>
-            <ReplaceKeyPrefixWith>category/itp-</ReplaceKeyPrefixWith>
-        </Redirect>
-    </RoutingRule>
-    <RoutingRule>
-        <Condition>
-            <KeyPrefixEquals>2010/01/27/einstein’s-theory-of-general-relativity-a-personal-description</KeyPrefixEquals>
-        </Condition>
-        <Redirect>
-            <ReplaceKeyPrefixWith>2010/01/27/einsteins-theory-of-general-relativity-a-personal-description</ReplaceKeyPrefixWith>
-        </Redirect>
-    </RoutingRule>
-</RoutingRules>
-```
 
 Front Matter Order
 ------------------
---
+++ print_routing_rules.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-\
+
+rule_format = """\t<RoutingRule>
+\t\t<Condition>
+\t\t\t<KeyPrefixEquals>%s</KeyPrefixEquals>
+\t\t</Condition>
+\t\t<Redirect>
+\t\t\t<HostName>lehrblogger.com</HostName>
+\t\t\t<ReplaceKeyPrefixWith>%s</ReplaceKeyPrefixWith>
+\t\t</Redirect>
+\t</RoutingRule>"""
+
+rules = [
+    ['nyu/projects', 'projects'],
+    ['nyu/events'  , 'projects'],
+    ['nyu/classes' , 'projects'],
+    ['category/ITP/4-in-4'           , 'category/itp-4-in-4'],
+    ['category/ITP/A2Z'              , 'category/itp-a2z'],
+    ['category/ITP/AJAX'             , 'category/itp-ajax'],
+    ['category/ITP/Design-For-UNICEF', 'category/itp-design-for-unicef'],
+    ['category/ITP/Election'         , 'category/itp-election'],
+    ['category/ITP/ICM'              , 'category/itp-icm'],
+    ['category/ITP/Little-Computers' , 'category/itp-little-computers'],
+    ['category/ITP/PComp'            , 'category/itp-pcomp'],
+    ['category/ITP/SocObj'           , 'category/itp-socobj'],
+    ['category/ITP/Time'             , 'category/itp-time'],
+    ['category/ITP/thesis'           , 'category/itp-thesis'],
+    ['category/ITP/'                 , 'category/itp/'],  # this one needs an explicit slash, otherwise it matches the correct categories
+    ['2010/01/27/einstein%e2%80%99s-theory-of-general-relativity-a-personal-description', '2010/01/27/einsteins-theory-of-general-relativity-a-personal-description']
+]
+
+if __name__ == '__main__':
+    print '<RoutingRules>'
+    for key_prefix_equals, replace_key_prefix_with in rules:
+        print rule_format % (key_prefix_equals, replace_key_prefix_with)
+        # Only lowercase if we need to, and don't lowercase if the point of the rule was to lowercase
+        if key_prefix_equals != key_prefix_equals.lower() and key_prefix_equals.lower() != replace_key_prefix_with:
+            print rule_format % (key_prefix_equals.lower(), replace_key_prefix_with)
+    print '</RoutingRules>'
